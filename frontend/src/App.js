d573fe8ce7e1ae9b21d0692ceb7bb0909aad624f
Daniel S. Waite <daniel.s.waite@aexp.com>
2023-03-12 16:05:44 -0700

    json database for backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from 'react';
+import { ringCard } from "./components/ringCard";
 
 const App = () =>{
+    const [rings, setRings] = useState([]);
+    
+    useEffect(() => {
+      fetch('http://localhost:3000/test', {mode:'no-cors'})
+        .then((res) => res.json())
+        .then((data) => {
+          setRings(data);
+        })
+
+        .catch((error) => {
+          console.error(error);
+        });
+    }, []);
+
     return (
-        <h1>
-            Hi Mom, look it's React!
-        </h1>
+        // <React.Fragment>
+        //     <h1>React</h1>
+        //     {rings.map((halo) => (
+        //         <h1>{halo.installation}</h1>
+        //     ))}
+        // </React.Fragment>
+
+        <React.Fragment>
+            <h1>The Best Person is: {rings}</h1>
+        </React.Fragment>
     )
 }
 
---
+++ frontend/src/components/ringCard.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function ringCard({props}) {
+  return (
+    <h1>ringCard {props.installation}</h1>
+  );
+}
+
+export { ringCard };
